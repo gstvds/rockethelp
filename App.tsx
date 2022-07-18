20ae45d2a7bcd1de7a791e76729613c42ed223d7
Gustavo Silva <gstvds@icloud.com>
2022-07-18 01:15:20 -0300

    feat: adds assets and theme. it also loads fonts and create loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/views/SignInView';
+import { Loading } from './src/components/Loading';
+
+import { theme } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={theme}>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
