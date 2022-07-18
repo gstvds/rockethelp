648563beadeabf6f5fc175b207f6c705b766e2f2
Gustavo Silva <gstvds@icloud.com>
2022-07-18 01:38:38 -0300

    feat: adds input component

---
+++ src/components/TextField.tsx
@@ -0,0 +1,24 @@
+import { Input, IInputProps } from "native-base";
+
+
+
+export function TextField({ ...rest }: IInputProps) {
+  return (
+    <Input
+      bg="gray.700"
+      height={14}
+      size="md"
+      borderWidth={0}
+      fontSize="md"
+      fontFamily="body"
+      color="white"
+      placeholderTextColor="gray.300"
+      _focus={{
+        borderWidth: 1,
+        borderColor: 'green.500',
+        bg: 'gray.700',
+      }}
+      {...rest}
+    />
+  )
+}
--- src/views/SignInView.tsx
+++ src/views/SignInView.tsx
@@ -1,14 +1,30 @@
-import { Heading, VStack } from "native-base";
+import { Heading, Icon, useTheme, VStack } from "native-base";
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 
+import { TextField } from "../components/TextField";
+
 export function SignInView() {
+  const { colors } = useTheme();
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
+      <TextField
+        placeholder="E-mail"
+        keyboardType="email-address"
+        mb={4}
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+      />
+      <TextField
+        placeholder="Senha"
+        type="password"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+      />
     </VStack>
   );
 }
