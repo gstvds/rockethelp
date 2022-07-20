d33ff00e593560b4bb60d6a5b1956c2cffdb8b6d
Gustavo Silva <gstvds@icloud.com>
2022-07-19 21:15:32 -0300

    feat: adds first version of DetailsView

---
+++ src/views/DetailsView.tsx
@@ -0,0 +1,14 @@
+import { VStack } from 'native-base';
+
+import { Header } from '../components/Header';
+
+export function DetailsView() {
+  return (
+    <VStack
+      flex={1}
+      bg="gray.700"
+    >
+      <Header title="Solicitação" />
+    </VStack>
+  );
+}
